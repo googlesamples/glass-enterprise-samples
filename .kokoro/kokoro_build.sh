d742ac72871d425c4009b59e89d26f518a3f1dc2
Joe Bolinger <jbolinger@google.com>
2020-03-19 17:25:18 -0700

    Force kokoro to download newer builds tools

--- .kokoro/kokoro_build.sh
+++ .kokoro/kokoro_build.sh
@@ -3,6 +3,9 @@
 # Fail on any error.
 set -e
 
+# Make sure the right tools are installed
+echo y | ${ANDROID_HOME}/tools/bin/sdkmanager "build-tools;28.0.3"
+
 cd ${KOKORO_ARTIFACTS_DIR}/github/glass-enterprise-samples
 
 ( cd Camera2Sample          && ./gradlew build )
