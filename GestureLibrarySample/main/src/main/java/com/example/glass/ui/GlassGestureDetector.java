105bd97fc8429752868790e64bff867b4dae48e8
Michal Idzkowski <idzkowski@google.com>
2020-03-31 09:54:34 -0700

    Added two finger gesture detection to the GlassGestureDetector

--- GestureLibrarySample/main/src/main/java/com/example/glass/ui/GlassGestureDetector.java
+++ GestureLibrarySample/main/src/main/java/com/example/glass/ui/GlassGestureDetector.java
@@ -19,6 +19,8 @@ package com.example.glass.ui;
 import android.content.Context;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.view.ViewConfiguration;
 
 /**
  * Gesture detector for Google Glass usage purposes.
@@ -30,10 +32,15 @@ public class GlassGestureDetector implements GestureDetector.OnGestureListener {
    */
   public enum Gesture {
     TAP,
+    TWO_FINGER_TAP,
     SWIPE_FORWARD,
+    TWO_FINGER_SWIPE_FORWARD,
     SWIPE_BACKWARD,
+    TWO_FINGER_SWIPE_BACKWARD,
     SWIPE_UP,
-    SWIPE_DOWN
+    TWO_FINGER_SWIPE_UP,
+    SWIPE_DOWN,
+    TWO_FINGER_SWIPE_DOWN
   }
 
   /**
@@ -72,11 +79,43 @@ public class GlassGestureDetector implements GestureDetector.OnGestureListener {
     }
   }
 
+  private static final int VELOCITY_UNIT = 1000;
+  private static final int FIRST_FINGER_POINTER_INDEX = 0;
+  private static final int SECOND_FINGER_POINTER_INDEX = 1;
+  private static final double TAN_ANGLE_DEGREES = Math.tan(Math.toRadians(60));
   static final int SWIPE_DISTANCE_THRESHOLD_PX = 100;
   static final int SWIPE_VELOCITY_THRESHOLD_PX = 100;
-  private static final double TAN_ANGLE_DEGREES = Math.tan(Math.toRadians(60));
 
-  private GestureDetector gestureDetector;
+  private final int touchSlopSquare;
+  /**
+   * This flag is set to true each time the {@link MotionEvent#ACTION_DOWN} action appears
+   * and it remains true until the finger moves out of the tap region.
+   * Checking of the finger movement takes place each time the {@link MotionEvent#ACTION_MOVE}
+   * action appears, until finger moves out of the tap region.
+   * If this flag is set to false, {@link Gesture#TAP} and {@link Gesture#TWO_FINGER_TAP}
+   * gestures won't be notified as detected.
+   *
+   * Tap region is calculated from the {@link ViewConfiguration#getScaledTouchSlop()} value.
+   * It prevents from detecting {@link Gesture#TAP} and {@link Gesture#TWO_FINGER_TAP} gestures
+   * during the scrolling on the touchpad.
+   */
+  private boolean isInTapRegion;
+  private boolean isTwoFingerGesture = false;
+  private boolean isActionDownPerformed = false;
+  private float firstFingerDownX;
+  private float firstFingerDownY;
+  private float firstFingerLastFocusX;
+  private float firstFingerLastFocusY;
+  private float firstFingerVelocityX;
+  private float firstFingerVelocityY;
+  private float firstFingerDistanceX;
+  private float firstFingerDistanceY;
+  private float secondFingerDownX;
+  private float secondFingerDownY;
+  private float secondFingerDistanceX;
+  private float secondFingerDistanceY;
+  private VelocityTracker velocityTracker;
+  private MotionEvent currentDownEvent;
   private OnGestureListener onGestureListener;
 
   /**
@@ -86,7 +125,8 @@ public class GlassGestureDetector implements GestureDetector.OnGestureListener {
    * @param onGestureListener is a listener for the gestures.
    */
   public GlassGestureDetector(Context context, OnGestureListener onGestureListener) {
-    gestureDetector = new GestureDetector(context, this);
+    final int touchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+    touchSlopSquare = touchSlop * touchSlop;
     this.onGestureListener = onGestureListener;
   }
 
@@ -98,10 +138,76 @@ public class GlassGestureDetector implements GestureDetector.OnGestureListener {
    * @return TRUE if event is handled by the Android {@link GestureDetector}. FALSE otherwise.
    */
   public boolean onTouchEvent(MotionEvent motionEvent) {
-    if (motionEvent.getActionMasked() == MotionEvent.ACTION_UP) {
-      onGestureListener.onTouchEnded();
+    if (velocityTracker == null) {
+      velocityTracker = VelocityTracker.obtain();
     }
-    return gestureDetector.onTouchEvent(motionEvent);
+    velocityTracker.addMovement(motionEvent);
+    boolean handled = false;
+
+    switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+      case MotionEvent.ACTION_DOWN:
+        firstFingerDownX = firstFingerLastFocusX = motionEvent.getX();
+        firstFingerDownY = firstFingerLastFocusY = motionEvent.getY();
+        isActionDownPerformed = true;
+        isInTapRegion = true;
+        if (currentDownEvent != null) {
+          currentDownEvent.recycle();
+        }
+        currentDownEvent = MotionEvent.obtain(motionEvent);
+        break;
+      case MotionEvent.ACTION_POINTER_DOWN:
+        isTwoFingerGesture = true;
+        secondFingerDownX = motionEvent.getX(motionEvent.getActionIndex());
+        secondFingerDownY = motionEvent.getY(motionEvent.getActionIndex());
+        break;
+      case MotionEvent.ACTION_MOVE:
+        final float firstFingerFocusX = motionEvent.getX(FIRST_FINGER_POINTER_INDEX);
+        final float firstFingerFocusY = motionEvent.getY(FIRST_FINGER_POINTER_INDEX);
+        final float scrollX = firstFingerLastFocusX - firstFingerFocusX;
+        final float scrollY = firstFingerLastFocusY - firstFingerFocusY;
+        firstFingerDistanceX = firstFingerFocusX - firstFingerDownX;
+        firstFingerDistanceY = firstFingerFocusY - firstFingerDownY;
+        if (motionEvent.getPointerCount() > 1) {
+          secondFingerDistanceX =
+              motionEvent.getX(SECOND_FINGER_POINTER_INDEX) - secondFingerDownX;
+          secondFingerDistanceY =
+              motionEvent.getY(SECOND_FINGER_POINTER_INDEX) - secondFingerDownY;
+        }
+        if (isInTapRegion) {
+          final float distance = (firstFingerDistanceX * firstFingerDistanceX)
+              + (firstFingerDistanceY * firstFingerDistanceY);
+          float distanceSecondFinger = 0;
+          if (motionEvent.getPointerCount() > 1) {
+            distanceSecondFinger = (secondFingerDistanceX * secondFingerDistanceX)
+                + (secondFingerDistanceY * secondFingerDistanceY);
+          }
+          if (distance > touchSlopSquare || distanceSecondFinger > touchSlopSquare) {
+            isInTapRegion = false;
+          }
+        }
+        if ((Math.abs(scrollX) >= 1) || (Math.abs(scrollY) >= 1)) {
+          handled = onGestureListener
+              .onScroll(currentDownEvent, motionEvent, scrollX, scrollY);
+          firstFingerLastFocusX = firstFingerFocusX;
+          firstFingerLastFocusY = firstFingerFocusY;
+        }
+        break;
+      case MotionEvent.ACTION_UP:
+        velocityTracker.computeCurrentVelocity(VELOCITY_UNIT);
+        firstFingerVelocityX = velocityTracker
+            .getXVelocity(motionEvent.getPointerId(motionEvent.getActionIndex()));
+        firstFingerVelocityY = velocityTracker
+            .getYVelocity(motionEvent.getPointerId(motionEvent.getActionIndex()));
+        handled = detectGesture();
+        onTouchEnded();
+        break;
+      case MotionEvent.ACTION_CANCEL:
+        velocityTracker.recycle();
+        velocityTracker = null;
+        isInTapRegion = false;
+        break;
+    }
+    return handled;
   }
 
   @Override
@@ -160,7 +266,7 @@ public class GlassGestureDetector implements GestureDetector.OnGestureListener {
 
     if (tan > TAN_ANGLE_DEGREES) {
       if (Math.abs(deltaY) < SWIPE_DISTANCE_THRESHOLD_PX
-              || Math.abs(velocityY) < SWIPE_VELOCITY_THRESHOLD_PX) {
+          || Math.abs(velocityY) < SWIPE_VELOCITY_THRESHOLD_PX) {
         return false;
       } else if (deltaY < 0) {
         return onGestureListener.onGesture(Gesture.SWIPE_UP);
@@ -169,7 +275,7 @@ public class GlassGestureDetector implements GestureDetector.OnGestureListener {
       }
     } else {
       if (Math.abs(deltaX) < SWIPE_DISTANCE_THRESHOLD_PX
-              || Math.abs(velocityX) < SWIPE_VELOCITY_THRESHOLD_PX) {
+          || Math.abs(velocityX) < SWIPE_VELOCITY_THRESHOLD_PX) {
         return false;
       } else if (deltaX < 0) {
         return onGestureListener.onGesture(Gesture.SWIPE_FORWARD);
@@ -177,5 +283,87 @@ public class GlassGestureDetector implements GestureDetector.OnGestureListener {
         return onGestureListener.onGesture(Gesture.SWIPE_BACKWARD);
       }
     }
+  }
+
+  private boolean detectGesture() {
+    if (!isActionDownPerformed) {
+      return false;
+    }
+    final double tan =
+        firstFingerDistanceX != 0 ? Math.abs(firstFingerDistanceY / firstFingerDistanceX)
+            : Double.MAX_VALUE;
+
+    if (isTwoFingerGesture) {
+      final double tanSecondFinger =
+          secondFingerDistanceX != 0 ? Math.abs(secondFingerDistanceY / secondFingerDistanceX)
+              : Double.MAX_VALUE;
+      return detectTwoFingerGesture(tan, tanSecondFinger);
+    } else {
+      return detectOneFingerGesture(tan);
+    }
+  }
+
+  private boolean detectOneFingerGesture(double tan) {
+    if (tan > TAN_ANGLE_DEGREES) {
+      if (Math.abs(firstFingerDistanceY) < SWIPE_DISTANCE_THRESHOLD_PX
+          || Math.abs(firstFingerVelocityY) < SWIPE_VELOCITY_THRESHOLD_PX) {
+        if (isInTapRegion) {
+          return onGestureListener.onGesture(Gesture.TAP);
+        }
+      } else if (firstFingerDistanceY < 0) {
+        return onGestureListener.onGesture(Gesture.SWIPE_UP);
+      } else if (firstFingerDistanceY > 0) {
+        return onGestureListener.onGesture(Gesture.SWIPE_DOWN);
+      }
+    } else {
+      if (Math.abs(firstFingerDistanceX) < SWIPE_DISTANCE_THRESHOLD_PX
+          || Math.abs(firstFingerVelocityX) < SWIPE_VELOCITY_THRESHOLD_PX) {
+        if (isInTapRegion) {
+          return onGestureListener.onGesture(Gesture.TAP);
+        }
+      } else if (firstFingerDistanceX < 0) {
+        return onGestureListener.onGesture(Gesture.SWIPE_FORWARD);
+      } else if (firstFingerDistanceX > 0) {
+        return onGestureListener.onGesture(Gesture.SWIPE_BACKWARD);
+      }
+    }
+    return false;
+  }
+
+  private boolean detectTwoFingerGesture(double tan, double tanSecondFinger) {
+    if (tan > TAN_ANGLE_DEGREES && tanSecondFinger > TAN_ANGLE_DEGREES) {
+      if (Math.abs(firstFingerDistanceY) < SWIPE_DISTANCE_THRESHOLD_PX
+          || Math.abs(firstFingerVelocityY) < SWIPE_VELOCITY_THRESHOLD_PX) {
+        if (isInTapRegion) {
+          return onGestureListener.onGesture(Gesture.TWO_FINGER_TAP);
+        }
+      } else if (firstFingerDistanceY < 0 && secondFingerDistanceY < 0) {
+        return onGestureListener.onGesture(Gesture.TWO_FINGER_SWIPE_UP);
+      } else if (firstFingerDistanceY > 0 && secondFingerDistanceY > 0) {
+        return onGestureListener.onGesture(Gesture.TWO_FINGER_SWIPE_DOWN);
+      }
+    } else {
+      if (Math.abs(firstFingerDistanceX) < SWIPE_DISTANCE_THRESHOLD_PX
+          || Math.abs(firstFingerVelocityX) < SWIPE_VELOCITY_THRESHOLD_PX) {
+        if (isInTapRegion) {
+          return onGestureListener.onGesture(Gesture.TWO_FINGER_TAP);
+        }
+      } else if (firstFingerDistanceX < 0 && secondFingerDistanceX < 0) {
+        return onGestureListener.onGesture(Gesture.TWO_FINGER_SWIPE_FORWARD);
+      } else if (firstFingerDistanceX > 0 && secondFingerDistanceX > 0) {
+        return onGestureListener.onGesture(Gesture.TWO_FINGER_SWIPE_BACKWARD);
+      }
+    }
+    return false;
+  }
+
+  private void onTouchEnded() {
+    isTwoFingerGesture = false;
+    if (velocityTracker != null) {
+      velocityTracker.recycle();
+      velocityTracker = null;
+    }
+    isActionDownPerformed = false;
+    onGestureListener.onTouchEnded();
   }
 }
