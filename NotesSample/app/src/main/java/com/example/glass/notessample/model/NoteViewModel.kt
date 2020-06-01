a596d3605725321a99a7827099cf01e8e5606a82
idzkowski-google <57770516+idzkowski-google@users.noreply.github.com>
2020-06-01 16:46:47 -0700

    Added persistent storage to the NotesSample application (#8)

---
+++ NotesSample/app/src/main/java/com/example/glass/notessample/model/NoteViewModel.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.glass.notessample.model
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.glass.notessample.storage.NoteRepository
+import com.example.glass.notessample.storage.NotesDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+/**
+ * View model for notes. Uses {@link NoteRepository} to communicate with the persistent storage.
+ */
+class NoteViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: NoteRepository
+    val allNotes: LiveData<List<Note>>
+
+    init {
+        val wordsDao = NotesDatabase.getDatabase(
+            application,
+            viewModelScope
+        ).wordDao()
+        repository = NoteRepository(wordsDao)
+        allNotes = repository.allNotes()
+    }
+
+    fun insert(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(note)
+    }
+
+    fun delete(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.delete(note)
+    }
+
+    fun find(text: String) = repository.find(text)
+}
