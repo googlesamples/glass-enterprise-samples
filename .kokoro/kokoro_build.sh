08819eee2db516e2027921920d6a301e6666e356
Michal Idzkowski <idzkowski@google.com>
2021-02-05 09:36:39 -0800

    Fixed formatting in kokoro_build.sh file

--- .kokoro/kokoro_build.sh
+++ .kokoro/kokoro_build.sh
@@ -9,13 +9,13 @@ ${ANDROID_HOME}/tools/bin/sdkmanager --install "ndk;21.0.6113669" "cmake;3.10.2.
 
 cd ${KOKORO_ARTIFACTS_DIR}/github/glass-enterprise-samples
 
-( cd Camera2Sample          && ./gradlew build )
-( cd CardSample             && ./gradlew build )
-( cd GallerySample          && ./gradlew build )
-( cd GestureLibrarySample   && ./gradlew build )
-( cd QRCodeScannerSample    && ./gradlew build )
-( cd VoiceRecognitionSample && ./gradlew build )
-( cd WebRTCSample           && ./gradlew build )
-( cd NotesSample           && ./gradlew build )
+( cd Camera2Sample                && ./gradlew build )
+( cd CardSample                   && ./gradlew build )
+( cd GallerySample                && ./gradlew build )
+( cd GestureLibrarySample         && ./gradlew build )
+( cd QRCodeScannerSample          && ./gradlew build )
+( cd VoiceRecognitionSample       && ./gradlew build )
+( cd WebRTCSample                 && ./gradlew build )
+( cd NotesSample                  && ./gradlew build )
 ( cd VoiceCommandsReloadingSample && ./gradlew build )
 
